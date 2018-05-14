26d1b7ec9de664cd22d56f784b76262e0940ed01
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-14 22:25:23 +0200

    Create && implement XMLParser

---
+++ expertsystem/src/main/java/com/codecool/XMLParser.java
@@ -0,0 +1,32 @@
+package com.codecool;
+
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public abstract class XMLParser {
+
+    public void loadXmlDocument(String xmlPath) {
+
+        try {
+            File fXmlFile = new File(xmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
