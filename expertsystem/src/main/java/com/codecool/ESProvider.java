85aa130e8a8d87a42ebb3b4fefa69e11ecbdcb6c
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-15 01:31:12 +0200

    Create && implement ESProvider class

---
+++ expertsystem/src/main/java/com/codecool/ESProvider.java
@@ -0,0 +1,28 @@
+package com.codecool;
+
+public class ESProvider {
+
+    private FactParser factParser;
+    private RuleParser ruleParser;
+
+
+    public ESProvider(FactParser factParser, RuleParser ruleParser) {
+        this.factParser = factParser;
+        this.ruleParser = ruleParser;
+    }
+
+
+    public void collectAnswers() {
+
+    }
+
+
+    public boolean getAnswerByQuestion(String questionId) {
+
+    }
+
+
+    public String evaluate() {
+
+    }
+}
