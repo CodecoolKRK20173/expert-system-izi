3169c0f955552a4f946f470ad757fd670d4ea9ef
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-15 22:53:26 +0200

    Implement FactParser

--- expertsystem/src/main/java/com/codecool/FactParser.java
+++ expertsystem/src/main/java/com/codecool/FactParser.java
@@ -1,46 +1,57 @@
 package com.codecool;
 
-
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
 
 public class FactParser extends XMLParser {
 
 
     FactRepository factRepository;
-    Document doc;
 
 
     public FactParser(String xmlPath) {
         this.factRepository = new FactRepository();
-        loadXmlDocument(xmlPath);
-        this.doc = getDocument();
     }
 
-    // FactRepository
-    public void getFactRepository() {
-        FactRepository fr = new FactRepository();
 
-        NodeList nList = doc.getElementsByTagName("country");
+    public FactRepository getFactRepository() {
 
-        for (int temp = 0; temp < nList.getLength(); temp++) {
+        FactRepository factRepository = new FactRepository();
 
-            Node nNode = nList.item(temp);
+        super.loadXmlDocument("Fact.xml");
+        NodeList factList = doc.getElementsByTagName("Fact");
 
-            System.out.println("\nCurrent Element :" + nNode.getNodeName());
-            Element eElement = (Element) nNode;
+        for (int i = 0; i < factList.getLength(); i++) {
+
+            Node node = factList.item(i);
+            Element factElement = (Element) node;
 
 
-            System.out.println("liq : " + eElement.getElementsByTagName("liquor").item(0).getTextContent());
+            NodeList descList = factElement.getElementsByTagName("Description");
 
-            Fact fact = new Fact(nNode.getNodeName(),"dupa");
-            boolean value = eElement.getElementsByTagName("liquor").item(0).getTextContent();
-            fact.setFactValueById("cheap", );
-            fact.set
+            Node descriptionNode = descList.item(0);
+            Element descriptionElement = (Element) descriptionNode;
+            String factId = factElement.getAttribute("id");
+            String descriptionValue = descriptionElement.getAttribute("value");
 
+            NodeList evalList = factElement.getElementsByTagName("Eval");
+
+            Fact fact = new Fact(factId, descriptionValue);
+
+            for (int k = 0; k < evalList.getLength(); k++){
+                Node evalNode = evalList.item(k);
+                Element evalElement = (Element) evalNode;
+                String evalText = evalElement.getTextContent();
+                String evalId = evalElement.getAttribute("id");
+                Boolean value = Boolean.valueOf(evalText);
+
+                fact.setFactValueById(evalId, value);
+            }
+            factRepository.addFact(fact);
         }
 
+        return factRepository;
     }
 }
