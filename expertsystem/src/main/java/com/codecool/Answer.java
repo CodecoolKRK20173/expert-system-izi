5b02125e9369d6b206de88f9a934c00f44631910
Pieczkowski <pieczkowski.t@gmail.com>
2018-05-16 12:19:32 +0200

    Implement Answer class

---
+++ expertsystem/src/main/java/com/codecool/Answer.java
@@ -0,0 +1,30 @@
+package com.codecool;
+
+import java.util.ArrayList;
+import java.util.IllegalFormatException;
+import java.util.List;
+
+public class Answer{
+
+    public List<Value> values = new ArrayList<>();
+
+    public void addValue(Value value){
+        this.values.add(value);
+    }
+
+    public boolean evaluateAnswerByInput(String input) throws IllegalArgumentException {
+
+            for (Value value : values) {
+                for (String word : value.getInputPattern()) {
+                    if (word.equals(input)) {
+                        return value.getSelectionType();
+                    }
+                }
+            }
+        throw new IllegalArgumentException();
+    }
+
+    public List<Value> getValues() {
+        return values;
+    }
+}
