575edb48493e59ff21e24d7f06b9e129f6f76502
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-16 10:10:54 +0200

    Create && implement QuestionIterator

---
+++ expertsystem/src/main/java/com/codecool/Iterators/QuestionIterator.java
@@ -0,0 +1,35 @@
+package com.codecool.Iterators;
+
+import com.codecool.Fact;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class QuestionIterator implements Iterator<Rule> {
+
+    private List<Rule> rules;
+    int indexPosition;
+
+
+    public FactIterator(List<Fact> facts) {
+        this.rulse = new ArrayList<>();
+        this.indexPosition = 0;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        if(this.rules.size() != indexPosition)
+            return true;
+
+        this.indexPosition = 0;
+        return false;
+    }
+
+
+    @Override
+    public Rule next() {
+        return this.rulse.get(indexPosition++);
+    }
+}
