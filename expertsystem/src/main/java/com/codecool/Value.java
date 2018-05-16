6104d0b60b8e51183ebb42d40aac68b786b05581
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-16 20:06:08 +0200

    Create private acces modifiers of fields

--- expertsystem/src/main/java/com/codecool/Value.java
+++ expertsystem/src/main/java/com/codecool/Value.java
@@ -1,16 +1,18 @@
 package com.codecool;
 
-
 import java.util.List;
+
 
 public abstract class Value {
 
-    boolean selectionType;
-    List<String> inputPattern;
+    private boolean selectionType;
+    private List<String> inputPattern;
+
 
     public List<String> getInputPattern(){
         return inputPattern;
     }
+
 
     public boolean getSelectionType(){
         return selectionType;
