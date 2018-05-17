267bfa7fa4dd45946d944a746dba948a78ecfd20
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-17 21:39:08 +0200

    Refactor QuestionIterator class'

--- expertsystem/src/main/java/com/codecool/Iterators/QuestionIterator.java
+++ expertsystem/src/main/java/com/codecool/Iterators/QuestionIterator.java
@@ -3,10 +3,6 @@ package com.codecool.Iterators;
 import com.codecool.Question;
 
 import java.util.Iterator;
-
-
-import com.codecool.Question;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +11,7 @@ public class QuestionIterator implements Iterator<Question> {
     private int index;
     private List<Question> questions;
 
+    
     public QuestionIterator(ArrayList<Question> questions) {
         this.index = 0;
         this.questions = questions;
