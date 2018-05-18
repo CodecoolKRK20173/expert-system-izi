9f3a9f6fbdb348fe3142b62bd3c918a33d212b5e
michaal111 <michalgrzegorczyk19@gmail.com>
2018-05-18 10:02:18 +0200

    Refactor Co-authored-by: Tomek <pieczkowski.t@gmail.com> Co-authored-by: Sev <s.talaj@gmail.com>

--- expertsystem/src/main/java/com/codecool/XMLParser.java
+++ expertsystem/src/main/java/com/codecool/XMLParser.java
@@ -6,6 +6,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
 import java.io.File;
 import java.io.IOException;
 
